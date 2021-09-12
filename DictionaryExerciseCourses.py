fc8386c84701551709c2ec37fa2a33e48bee915d
Annabelle Herschend <annabelle_herschend1@baylor.edu>
2021-09-12 11:19:06 -0500

    Mid-Project

---
+++ DictionaryExerciseCourses.py
@@ -0,0 +1,44 @@
+CourseInformation = {
+    'CS101': {
+        'Room Number': '3004',
+        'Instructor': 'Haynes',
+        'Meeting Time': '8:00 a.m.'
+
+    },
+    'CS102': {
+        'Room Number': '4501',
+        'Instructor': 'Alvarado',
+        'Meeting Time': '9:00 a.m.'
+    },
+    'CS103': {
+        'Room Number': '6755',
+        'Instructor': 'Rich',
+        'Meeting Time': '10:00 a.m.'
+    },
+    'NT110': {
+        'Room Number': '1244',
+        'Instructor': 'Burke',
+        'Meeting Time': '11:00 a.m.'
+    },
+    'CM241': {
+        'Room Number': '1411',
+        'Instructor': 'Lee',
+        'Meeting Time': '1:00 p.m.'
+    }
+}
+
+course_num = input('What course number are you inquiring about?')
+print()
+
+print('Course Number: ', course_num)
+print('Room Number: ', CourseInformation[course_num]['Room Number'])
+print('Instructor: ', CourseInformation[course_num]['Instructor'])
+print('Meeting Time: ', CourseInformation[course_num]['Meeting Time'])
+
+print()
+
+
+
+
+
+
